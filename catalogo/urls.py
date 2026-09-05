ba2ffa7d1d50d372346a3c2f6f47671a68b820bf
Marco Milanca <milancamarco694@gmail.com>
2026-09-05 19:48:36 -0400

    etapa-2-interfaz

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.lista_productos, name='lista_productos'),
+    path('producto/<int:id>/', views.detalle_producto, name='detalle_producto'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,5 +1,23 @@
-from django.http import HttpResponse
+import json
+from pathlib import Path
+from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
-def inicio(request):
-    return HttpResponse("¡Bienvenido a la página de inicio del catálogo!")
+RUTA_JSON = Path(__file__).resolve().parent.parent / "data" / "productos.json"
+
+def cargar_productos():
+    with open(RUTA_JSON, encoding="utf-8") as f:
+        return json.load(f)
+
+def lista_productos(request):
+    productos = cargar_productos()
+    return render(request, "catalogo/lista.html", {"productos": productos})
+
+def detalle_producto(request, id):
+    productos = cargar_productos()
+    producto = next((p for p in productos if p["id"] == id), None)
+    
+    if not producto:
+        raise Http404("Producto no encontrado")
+        
+    return render(request, "catalogo/detalle.html", {"producto": producto})
